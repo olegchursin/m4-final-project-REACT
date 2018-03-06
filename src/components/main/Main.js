e14617fcc9f18f2ce8367d013135c8157b25102d
olegchursin <olegchursin@gmail.com>
2018-03-06 12:13:43 -0500

    add scroll to anchor

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -2,15 +2,20 @@ import React from 'react'
 import Hero from './Hero'
 import MainBeerList from './MainBeerList'
 import MainBreweriesList from './MainBreweriesList'
+import ScrollableAnchor from 'react-scrollable-anchor'
 
 const Main = (props) => {
   return (
     <div>
       <Hero />
       <div className="ui container">
-        <h2 id="main-beers">Beers</h2>
+        <ScrollableAnchor id={'main-beers'}>
+          <h2>Beers</h2>
+        </ScrollableAnchor>
         <MainBeerList beers={props.beers} />
-        <h2 id="main-breweries">Breweries</h2>
+        <ScrollableAnchor id={'main-breweries'}>
+          <h2>Breweries</h2>
+        </ScrollableAnchor>
         <MainBreweriesList breweries={props.breweries} />
       </div>
 
